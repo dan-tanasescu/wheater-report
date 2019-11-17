59a6c5ce8b792aca0983a3f5656356364e26ce0d
dan-tanasescu <dantasse@yahoo.com>
2019-11-17 21:15:45 +0200

    Renamed some variables with their English counterparts for better clarity of code.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Creating the XMLHttpRequest object conditionally for IE browser compatibility.
 
 var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-console.log("Hello World!")
 
 // Using a proxy for fixing the CORS error.
 
@@ -14,29 +13,29 @@ request.send();
 request.addEventListener("load", function() {
     if(this.readyState === 4 && this.status === 200) {
     var obj = JSON.parse(this.response);
-    var afisaj = document.getElementById("container");
-    var afisaj_lista = document.getElementById("list_container");
-    var continut = "<select id='lista'><option value=''>Alegeți orașul</option>";
+    var viewer = document.getElementById("container");
+    var places_list = document.getElementById("list_container");
+    var report = "<select id='list'><option value=''>Alegeți orașul</option>";
     for(var i = 0; i < 40; i++){
-        continut += "<option value='" + i + "'>" + obj.features[i].properties.nume + "</option>";
+        report += "<option value='" + i + "'>" + obj.features[i].properties.nume + "</option>";
   
   }
-  continut += "</select>";
-  afisaj_lista.innerHTML = continut;
-  var selectie = document.getElementById("lista");
-  selectie.addEventListener("change", function(){
-  var vreme = "<p><b>Localitate: </b>" + obj.features[lista.value].properties.nume + "</p>";
-  vreme += "<p><b>Actualizat: </b>" + obj.features[lista.value].properties.actualizat + "</p>";
-  vreme += "<p><b>Temperatură: </b>" + obj.features[lista.value].properties.tempe + "</p>";
-  vreme += "<p><b>Umezeală: </b>" + obj.features[lista.value].properties.umezeala + "</p>";
-  vreme += "<p><b>Nebulozitate: </b>" + obj.features[lista.value].properties.nebulozitate + "</p>";
-  vreme += "<p><b>Presiune: </b>" + obj.features[lista.value].properties.presiunetext + "</p>";
-  vreme += "<p><b>Vânt: </b>" + obj.features[lista.value].properties.vant + "</p>";
-  vreme += "<p><b>Fenomene: </b>" + obj.features[lista.value].properties.fenomen_e + "</p>";
-  vreme += "<p><b>Zăpadă: </b>" + obj.features[lista.value].properties.zapada + "</p>";
-  vreme += "<p><b>Temperatură apă: </b>" + obj.features[lista.value].properties.tempapa + "</p>";
-  afisaj.innerHTML = vreme;
-  afisaj.style.borderColor = "#000";
+  report += "</select>";
+  places_list.innerHTML = report;
+  var selection = document.getElementById("list");
+  selection.addEventListener("change", function(){
+  var wheater = "<p><b>Localitate: </b>" + obj.features[list.value].properties.nume + "</p>";
+  wheater += "<p><b>Actualizat: </b>" + obj.features[list.value].properties.actualizat + "</p>";
+  wheater += "<p><b>Temperatură: </b>" + obj.features[list.value].properties.tempe + "</p>";
+  wheater += "<p><b>Umezeală: </b>" + obj.features[list.value].properties.umezeala + "</p>";
+  wheater += "<p><b>Nebulozitate: </b>" + obj.features[list.value].properties.nebulozitate + "</p>";
+  wheater += "<p><b>Presiune: </b>" + obj.features[list.value].properties.presiunetext + "</p>";
+  wheater += "<p><b>Vânt: </b>" + obj.features[list.value].properties.vant + "</p>";
+  wheater += "<p><b>Fenomene: </b>" + obj.features[list.value].properties.fenomen_e + "</p>";
+  wheater += "<p><b>Zăpadă: </b>" + obj.features[list.value].properties.zapada + "</p>";
+  wheater += "<p><b>Temperatură apă: </b>" + obj.features[list.value].properties.tempapa + "</p>";
+  viewer.innerHTML = wheater;
+  viewer.style.borderColor = "#000";
   });
   
 }
